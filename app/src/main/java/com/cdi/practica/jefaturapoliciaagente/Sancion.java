c7a385c9f147ef0d379c2bbd70c771ce165fcdbd
Sergio López Ceballos <sergio.lceballos@alumnos.upm.es>
2018-07-08 20:14:04 +0200

    Mejoras diseño sanciones

--- app/src/main/java/com/cdi/practica/jefaturapoliciaagente/Sancion.java
+++ app/src/main/java/com/cdi/practica/jefaturapoliciaagente/Sancion.java
@@ -4,19 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Sancion extends AppCompatActivity {
+
+    private Spinner tipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sancion);
 
+        tipo = (Spinner) findViewById(R.id.t2);
+        String[] hechos = { "Exceso velocidad", "Aparcamiento", "Altercado público",
+                "Posesión de drogas", "Robo"};
+        tipo.setAdapter(new ArrayAdapter<String>(this, R.layout.my_spinner_item, hechos));
+
+
         findViewById(R.id.t8).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Sancion.this,MainActivity.class));
+                startActivity(new Intent(Sancion.this, MainActivity.class));
                 finish();
+                Toast.makeText(getApplicationContext(), "Sanción enviada", Toast.LENGTH_SHORT).show();
             }
         });
     }
